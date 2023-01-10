9c548959ab8ea760b0ef00d074eedadf4fe71cde
siminaguta <122386300+siminaguta@users.noreply.github.com>
2023-01-10 19:05:23 +0200

    Add project files.

---
+++ proiectMAP/prmap.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string numar_cifre_romane(int n) {
+	int valori[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+	string simboluri[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" }; //toate simbolurile posibile pentru cifrele romane
+	string rezultat = "";
+
+	int i = 0;
+	while (n > 0) {
+		for (int j = n / valori[i]; j > 0; j--) {
+			rezultat += simboluri[i];
+			n -= valori[i];
+		}
+		i++;
+	}
+	return rezultat;
+}
+
+int main() {
+	int numar;
+	cout << "Dati un numar: ";
+	cin >> numar;
+	cout << "Numar tradus in cifre romane este: " << numar_cifre_romane(numar); 
+	return 0;
+}
