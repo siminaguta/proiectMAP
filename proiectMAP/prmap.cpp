aab688e510e79ad1b22d62b149f2393069b1f35d
siminaguta <122386300+siminaguta@users.noreply.github.com>
2023-01-10 19:08:22 +0200

    Update prmap.cpp

--- proiectMAP/prmap.cpp
+++ proiectMAP/prmap.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 string numar_cifre_romane(int n) {
-	int valori[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-	string simboluri[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" }; //toate simbolurile posibile pentru cifrele romane
+	int valori[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };						
+	//toate simbolurile posibile pentru cifrele romane
+	string simboluri[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" }; 
 	string rezultat = "";
 
 	int i = 0;
